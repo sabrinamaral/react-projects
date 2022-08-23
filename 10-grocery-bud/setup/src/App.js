4a19788afbc3f2d944dad7a89f0d6898847d939c
sabrinamaral <sabrinamaral@hotmail.com>
2022-08-22 22:15:39 -0300

    end of grocery bud project

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -2,9 +2,18 @@ import React, { useState, useEffect } from "react";
 import List from "./List";
 import Alert from "./Alert";
 
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    return JSON.parse(localStorage.getItem("list"));
+  } else {
+    return [];
+  }
+};
+
 function App() {
   const [name, setName] = useState("");
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStorage());
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({
@@ -18,7 +27,18 @@ function App() {
     if (!name) {
       showAlert(true, "please enter some value", "danger");
     } else if (name && isEditing) {
-      // setIsEditing()
+      setList(
+        list.map((item) => {
+          if (item.id === editID) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+      setName("");
+      setEditID(null);
+      setIsEditing(false);
+      showAlert(true, "value changed", "success");
     } else {
       const newItem = {
         id: new Date().getTime().toString(),
@@ -36,6 +56,15 @@ function App() {
     showAlert(true, "item removed", "danger");
     setList(list.filter((item) => item.id !== id));
   };
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditID(id);
+    setName(specificItem.title);
+  };
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
@@ -56,7 +85,12 @@ function App() {
       </form>
       {list.length > 0 && (
         <div className="grocery-container">
-          <List items={list} setItems={setList} removeItem={removeItem} />
+          <List
+            items={list}
+            setItems={setList}
+            removeItem={removeItem}
+            editItem={editItem}
+          />
           <button
             className="clear-btn"
             onClick={() => {
