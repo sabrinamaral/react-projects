7e1bdb97462d44b2fabbb74bb0123b77317566dd
sabrinamaral <sabrinamaral@hotmail.com>
2022-08-30 17:22:10 -0300

    cart project

---
+++ 14-cart/setup/src/reducer.js
@@ -0,0 +1,33 @@
+const reducer = (state, action) => {
+  if (action.type === "CLEAR_CART") {
+    return { ...state, cart: [] };
+  }
+  if (action.type === "REMOVE_ITEM") {
+    const newList = state.cart.filter((item) => item.id !== action.payload);
+    return { ...state, cart: newList };
+  }
+  if (action.type === "INCREASE") {
+    let tempCart = state.cart.map((item) => {
+      if (item.id === action.payload) {
+        return { ...item, amount: item.amount + 1 };
+      }
+      return item;
+    });
+    return { ...state, cart: tempCart };
+  }
+
+  if (action.type === "DECREASE") {
+    let tempCart = state.cart
+      .map((item) => {
+        if (item.id === action.payload) {
+          return { ...item, amount: item.amount - 1 };
+        }
+        return item;
+      })
+      .filter((item) => item.amount !== 0);
+    return { ...state, cart: tempCart };
+  }
+  throw new Error("no action type matches with the action type provided");
+};
+
+export default reducer;
