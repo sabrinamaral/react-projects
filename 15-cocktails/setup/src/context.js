a419fda87d27d9601dcd76fcf11f705694b7d6c6
sabrinamaral <sabrinamaral@hotmail.com>
2022-09-08 12:55:42 -0300

    add useCallback to context

--- 15-cocktails/setup/src/context.js
+++ 15-cocktails/setup/src/context.js
@@ -9,7 +9,7 @@ const AppProvider = ({ children }) => {
   const [searchTerm, setSearchTerm] = useState("a");
   const [cocktails, setCocktails] = useState([]);
 
-  const fetchData = async () => {
+  const fetchData = useCallback(async () => {
     setLoading(true);
     try {
       const response = await fetch(`${url}${searchTerm}`);
@@ -36,11 +36,11 @@ const AppProvider = ({ children }) => {
       console.log(error);
       setLoading(false);
     }
-  };
+  }, [searchTerm]);
 
   useEffect(() => {
     fetchData();
-  }, [searchTerm]);
+  }, [searchTerm, fetchData]);
 
   return (
     <AppContext.Provider value={{ loading, cocktails, setSearchTerm }}>
