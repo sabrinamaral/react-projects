345e927e4bb2de83184c6977dc6f63925bcc1f26
sabrinamaral <sabrinamaral@hotmail.com>
2022-09-08 11:46:43 -0300

    cocktails project | home, about, error pages

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, ut
+        corporis laudantium saepe accusamus similique labore maiores doloribus
+        voluptatum nobis non dicta velit perspiciatis voluptatem tempore fuga
+        possimus molestias, autem, hic magni eveniet eius. Sequi itaque ad
+        reiciendis laborum perspiciatis distinctio placeat officia maiores iusto
+        debitis. Reiciendis impedit ducimus assumenda?
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
