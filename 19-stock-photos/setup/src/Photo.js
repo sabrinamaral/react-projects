2aa8cd5d819c9a9b279290c783d78249b46ab40d
sabrinamaral <sabrinamaral@hotmail.com>
2022-09-18 09:12:50 -0300

    stock_photos project | minor adjustments

--- 19-stock-photos/setup/src/Photo.js
+++ 19-stock-photos/setup/src/Photo.js
@@ -12,14 +12,14 @@ const Photo = ({
 }) => {
   return (
     <article className="photo">
-      <img src={regular} alt={alt_description} />;
+      <img src={regular} alt={alt_description} />
       <div className="photo-info">
         <div>
           <h4>{name}</h4>
           <p>{likes} likes</p>
         </div>
         <a href={portfolio_url}>
-          <img src={medium} alt={name} className="user-img" />
+          <img src={medium} alt="" className="user-img" />
         </a>
       </div>
     </article>
