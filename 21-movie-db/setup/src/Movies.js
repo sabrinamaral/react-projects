5bda11d0eb2efb571da9379e17b332e2aaa7d789
sabrinamaral <sabrinamaral@hotmail.com>
2022-09-22 12:07:06 -0300

    Movie DB project | components

--- 21-movie-db/setup/src/Movies.js
+++ 21-movie-db/setup/src/Movies.js
@@ -1,11 +1,39 @@
-import React from 'react'
-import { useGlobalContext } from './context'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { useGlobalContext } from "./context";
+import { Link } from "react-router-dom";
 const url =
-  'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png'
+  "https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png";
 
 const Movies = () => {
-  return <h2>movies component</h2>
-}
+  const { loading, movies } = useGlobalContext();
 
-export default Movies
+  if (loading) {
+    return <div className="loading"></div>;
+  } else {
+    return (
+      <section className="movies">
+        {movies.map((movie) => {
+          const {
+            Title: title,
+            Year: year,
+            Poster: poster,
+            imdbID: id,
+          } = movie;
+          return (
+            <Link to={`/movies/${id}`} key={id} className="movie">
+              <article>
+                <img src={poster === "N/A" ? url : poster} alt={title} />
+                <div className="movie-info">
+                  <h4>{title}</h4>
+                  <p>{year}</p>
+                </div>
+              </article>
+            </Link>
+          );
+        })}
+      </section>
+    );
+  }
+};
+
+export default Movies;
