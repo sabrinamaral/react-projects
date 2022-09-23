68b9baab21dd77d91be1b277ac7c4d2cf96e84b6
sabrinamaral <sabrinamaral@hotmail.com>
2022-09-23 16:51:08 -0300

    hacker news | stories component

--- 22-hacker-news/setup/src/Stories.js
+++ 22-hacker-news/setup/src/Stories.js
@@ -1,9 +1,45 @@
-import React from 'react'
-
-import { useGlobalContext } from './context'
+import React from "react";
+import { useGlobalContext } from "./context";
 
 const Stories = () => {
-  return <h2>stories component</h2>
-}
+  const { isLoading, hits } = useGlobalContext();
+  if (isLoading) {
+    return <div className="loading"></div>;
+  }
+  return (
+    <section className="stories">
+      {hits.map((story) => {
+        const {
+          title,
+          objectID: id,
+          num_comments,
+          url,
+          points,
+          author,
+        } = story;
+        return (
+          <article className="story" key={id}>
+            <h4 className="title">{title}</h4>
+            <p className="info">
+              {points} points by <span>{author} | </span>
+              {num_comments} comments
+            </p>
+            <div>
+              <a
+                href={url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="read-link"
+              >
+                read more
+              </a>
+              <button className="remove-btn">remove</button>
+            </div>
+          </article>
+        );
+      })}
+    </section>
+  );
+};
 
-export default Stories
+export default Stories;
